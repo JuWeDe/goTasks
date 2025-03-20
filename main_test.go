6516d19f8bd9c732c4ddb21fcd4402bfb21d220f
roman <roman18011998@gmail.com>
2025-03-20 16:44:49 +0300

    добавлены тесты

---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestReverseNumber(t *testing.T) {
+    result := reverseNumber(843)
+    if result != 348 {
+        t.Errorf("Ожидалось 348, получено %d", result)
+    }
+}
+
+func TestIsRightTriangle(t *testing.T) {
+    result := isRightTriangle(6, 8, 10)
+    if result != "Прямоугольный" {
+        t.Errorf("Ожидалось 'Прямоугольный', получено '%s'", result)
+    }
+}
+
+func TestTimeFromSeconds(t *testing.T) {
+    hours, minutes := timeFromSeconds(13257)
+    if hours != 3 || minutes != 40 {
+        t.Errorf("Ожидалось 3 часа 40 минут, получено %d часов %d минут", hours, minutes)
+    }
+}
